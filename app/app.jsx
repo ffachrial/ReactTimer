e66b03fab945e55f4fd123c1f231b33d1bbdea93
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-24 09:30:11 +0700

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -10,11 +10,15 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 import Main from 'Main';
+import Countdown from 'Countdown';
+import Timer from 'Timer';
 
 ReactDOM.render(
   // Component will Render
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById('app')
