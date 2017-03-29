4a359fac17552cdd922d7e17644d6f6f74189b1d
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-29 10:48:34 +0700

    Add countdown functionality

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -11,25 +11,27 @@ describe('CountdownForm', () => {
     expect(CountdownForm).toExist();
   });
 
-  it('should call onSetCountdown if valid seconds entered', () => {
-    var spy = expect.createSpy();
-    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(countdownForm));
+  describe('onSetCountdown', () => {
+    it('should call onSetCountdown if valid seconds entered', () => {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(countdownForm));
 
-    countdownForm.refs.seconds.value = '109';
-    TestUtils.Simulate.submit($el.find('form')[0]);
+      countdownForm.refs.seconds.value = '109';
+      TestUtils.Simulate.submit($el.find('form')[0]);
 
-    expect(spy).toHaveBeenCalledWith(109);
-  });
+      expect(spy).toHaveBeenCalledWith(109);
+    });
 
-  it('should not call onSetCountdown if invalid seconds entered', () => {
-    var spy = expect.createSpy();
-    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(countdownForm));
+    it('should not call onSetCountdown if invalid seconds entered', () => {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(countdownForm));
 
-    countdownForm.refs.seconds.value = 'abc';
-    TestUtils.Simulate.submit($el.find('form')[0]);
+      countdownForm.refs.seconds.value = 'abc';
+      TestUtils.Simulate.submit($el.find('form')[0]);
 
-    expect(spy).toNotHaveBeenCalled();
+      expect(spy).toNotHaveBeenCalled();
+    });
   });
 });
