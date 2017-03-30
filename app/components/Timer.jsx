685523dd51b568b83cc93695cf0034e8b0cd2182
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-30 17:04:24 +0700

    Created timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import Clock from 'Clock';
+import Controls from 'Controls';
+
 export default class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.handleStart();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer);
+  }
+
+  handleStart () {
+    this.timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  }
+
+  handleStatusChange (newTimerStatus) {
+    this.setState({timerStatus: newTimerStatus});
+  }
+
   render () {
+    var {count, timerStatus} = this.state;
+
     return (
-      <p>Timer.jsx Rendered</p>
+      <div>
+        <h1 className='page-title'>Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange.bind(this)}/>
+      </div>
     );
   }
 };
